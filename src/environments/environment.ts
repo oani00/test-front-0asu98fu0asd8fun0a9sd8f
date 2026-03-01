f6401c18fcb9256218bf8cf1bfd784e306747244
Oaní da Silva da Costa <oani_costa@outlook.com.br>
2026-03-01 20:19:40 -0300

    change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://test-back-sdpofjasoifjaosdifjsaddfm-i64of6rw8-oanis-projects.vercel.app'
+  apiUrl: 'https://test-back-sdpofjasoifjaosdifjsaddfm.vercel.app/'
 };
